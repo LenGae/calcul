00691c0bc53ed9bf451b658916fad06b481db3a4
Лейман Станислав Эдуардович <LeymanSE@dom201.local>
2026-09-09 10:35:53 +0700

    def /

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ b = int(input("Число 2: "))
 op = input("Знак: ")
 
 class Calculator:
-    def st(self, a, b):
-        return a-b
+    def pod(self, a, b):
+        if b == 0:
+            return "На ноль нельзя делить"
+        return a/b
 
 calculator = Calculator()
 
-if op == "-":
-    res = calculator.st(a, b)
-
+if op == "/":
+    res = calculator.pod(a, b)
 
 print(res)
